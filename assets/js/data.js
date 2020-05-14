9880643655973f68092dd3ae278ff76a47f5c2a0
Tionis Nagir <tionis@tasadar.net>
2020-05-14 14:52:48 +0200

    Added loading of bookmark links from localStorage

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,15 +1,24 @@
 var data_links = "links.json";
+var bookmarks = JSON.parse(localStorage.getItem("links"));
+
+function handleLinks(data) {
+  var mysource = document.getElementById("links-template").innerHTML;
+  var mytemplate = Handlebars.compile(mysource);
+  var myresult = mytemplate(data)
+  document.getElementById("links").innerHTML = myresult;
+}
 
 document.addEventListener("DOMContentLoaded", function () {
-  fetch(data_links)
-  .then( response => response.json())
-  .then(
-    function (data) {
-      var mysource = document.getElementById("links-template").innerHTML;
-      var mytemplate = Handlebars.compile(mysource);
-      var myresult = mytemplate(data)
-      document.getElementById("links").innerHTML = myresult;
-    });
+  if (!bookmarks) {
+      fetch(data_links)
+          .then(response => response.json())
+          .then(function (data) {
+              handleLinks(data);
+              localStorage.setItem("links", JSON.stringify(data));
+          });
+  } else {
+      handleLinks(bookmarks);
+  }
 });
 
 var data_apps = "apps.json";
