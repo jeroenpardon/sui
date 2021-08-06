15898ce074b1394cf20bcc5375bd5f35a17f1900
dannytszho <76494181+dannytszho@users.noreply.github.com>
2021-08-06 16:20:25 -0700

    Added weather feature but still need more styling.

---
+++ assets/js/weatherApp.js
@@ -0,0 +1,43 @@
+let weather = {
+    "apikey": "375090722ff9b13931a9ed44d6f32f14",
+
+    // Fetch data from openweathermap API call.
+    fetchWeather: function (city) {
+        fetch(
+            "http://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid="
+            + this.apikey
+    )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    // Utilize data from the API call
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+          "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+
+    },
+    search: function (){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+// Allow users to search with "Enter" button
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
