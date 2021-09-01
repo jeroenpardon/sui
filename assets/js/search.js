7b1b2985ad0342fdff7cccce08c8787a93310fa0
k <kori@tulibu-dibu.com>
2021-09-01 20:05:39 +0300

    Variable for default search engine
    Added a variable for default search engine (leaving Google for compatibility reasons), and added a case for Google if something else is defined.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,6 @@
 var sindex = 0;
 var cycle = false;
+var sengine = "https://www.google.com/?q="; // Default search engine
 
 function start() {
     var query = getParameterByName('q');
@@ -92,6 +93,10 @@ function search(text) {
                 case "y":
                     window.location = "https://www.youtube.com/results?search_query=" + subtext;
                     break;
+                case "g":
+                    window.location = "https://www.google.com/?q=" + subtext;
+                    break;
+
             }
         } else {
             var option = text.substr(1);
